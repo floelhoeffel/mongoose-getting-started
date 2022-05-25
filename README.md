db21174bad084979482ad6469434f1356c83e078
Frederik Lölhöffel <6864947+floelhoeffel@users.noreply.github.com>
2022-05-25 16:21:39 +0200

    script working

---
+++ README.md
@@ -0,0 +1,5 @@
+docker run --name mongoose-getting-started -d mongo
+
+mongodb://localhost:27017/mongoose-getting-started
+
+mongoose-getting-started
---
+++ getting-started.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect(
+    "mongodb://root:secret@localhost:27017/mongoose-getting-started?authSource=admin"
+  );
+
+  const kittySchema = new mongoose.Schema({
+    name: String,
+  });
+
+  kittySchema.methods.speak = function speak() {
+    const greeting = this.name
+      ? "Meow name is " + this.name
+      : "I don't have a name";
+    console.log(greeting);
+  };
+
+  const Kitten = mongoose.model("Kitten", kittySchema);
+
+  const silence = new Kitten({ name: "Silence" });
+  //   console.log(silence.name); // 'Silence'
+
+  const fluffy = new Kitten({ name: "fluffy" });
+  //   fluffy.speak(); // "Meow name is fluffy"
+
+  await silence.save();
+  await fluffy.save();
+
+  return;
+}
